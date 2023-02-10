477069e2aeaa7fc210481b9b4caa44c68c6f27c5
segfaultdoctor <17258903+segfaultdoc@users.noreply.github.com>
2023-02-10 16:14:27 -0700

    genesis

---
+++ README.md
@@ -0,0 +1,2 @@
+# bundle-guard
+Simple on-chain program that's meant to wrap the inner transactions of a bundle, aborting if the signer's balance is negative.
---
+++ programs/bundle-guard/src/lib.rs
@@ -0,0 +1,116 @@
+use std::mem::size_of;
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+/// NOTE: This program is meant to only be used in the context of bundles.
+/// The pre/post guard instructions should be separate transactions wrapping the inner contents of a bundle.
+#[program]
+pub mod bundle_guard {
+    use super::*;
+
+    pub fn close_guard_state(_ctx: Context<CloseGuardState>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn pre_guard(ctx: Context<PreGuard>, bump: u8) -> Result<()> {
+        let guarded_state = &mut ctx.accounts.guarded_state;
+        guarded_state.lamports = ctx.accounts.guarded_account.lamports();
+        guarded_state.bump = bump;
+
+        Ok(())
+    }
+
+    pub fn post_guard(ctx: Context<PostGuard>) -> Result<()> {
+        if ctx.accounts.guarded_account.lamports() < ctx.accounts.guarded_state.lamports {
+            Err(Error::AnchorError(AnchorError {
+                error_name: "guard failure".to_string(),
+                error_code_number: 69,
+                error_msg: format!("negative balance change: pre lamports: {}, post lamports: {}", ctx.accounts.guarded_account.lamports(), ctx.accounts.guarded_state.lamports),
+                error_origin: None,
+                compared_values: None
+            }))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[derive(Accounts)]
+pub struct CloseGuardState<'info> {
+    /// CHECK: We just care about the account's lamports.
+    pub guarded_account: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            GuardedState::SEED,
+            guarded_account.key().as_ref(),
+            signer.key().as_ref(),
+        ],
+        bump = guarded_state.bump,
+        close = signer
+    )]
+    pub guarded_state: Account<'info, GuardedState>,
+
+    /// Anyone can crank this instruction.
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct PreGuard<'info> {
+    /// CHECK: We just care about the account's lamports.
+    pub guarded_account: AccountInfo<'info>,
+
+    #[account(
+        init_if_needed,
+        seeds = [
+            GuardedState::SEED,
+            guarded_account.key().as_ref(),
+            signer.key().as_ref(),
+        ],
+        bump,
+        space = GuardedState::SIZE,
+        payer = signer
+    )]
+    pub guarded_state: Account<'info, GuardedState>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct PostGuard<'info> {
+    /// CHECK: We just care about the account's lamports.
+    pub guarded_account: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            GuardedState::SEED,
+            guarded_account.key().as_ref(),
+            signer.key().as_ref(),
+        ],
+        bump = guarded_state.bump,
+        close = signer
+    )]
+    pub guarded_state: Account<'info, GuardedState>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct GuardedState {
+    pub lamports: u64,
+    pub bump: u8,
+}
+
+impl GuardedState {
+    pub const SEED: &'static [u8] = b"GUARDED_STATE";
+    pub const SIZE: usize = 8 + size_of::<Self>();
+}
