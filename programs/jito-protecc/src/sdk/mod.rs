7a5b633439fb8e81370278460d64da0b0fed57e8
segfaultdoctor <17258903+segfaultdoc@users.noreply.github.com>
2023-02-13 18:16:53 -0500

    add separate sol guards

--- programs/jito-protecc/src/sdk/mod.rs
+++ programs/jito-protecc/src/sdk/mod.rs
@@ -93,3 +93,97 @@ pub fn close_token_guarded_state_ix(
         .to_account_metas(None),
     }
 }
+
+pub struct PreSolGuardArgs {
+    pub bump: u8,
+}
+pub struct PreSolGuardAccounts {
+    pub sol_guarded_state: Pubkey,
+    pub signer: Pubkey,
+    pub system_program: Pubkey,
+    pub guarded_account: Pubkey,
+}
+pub fn pre_sol_guard_ix(
+    program_id: Pubkey,
+    args: PreSolGuardArgs,
+    accounts: PreSolGuardAccounts,
+) -> Instruction {
+    let PreSolGuardArgs { bump } = args;
+
+    let PreSolGuardAccounts {
+        sol_guarded_state,
+        signer,
+        system_program,
+        guarded_account,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: crate::instruction::PreSolGuard { bump }.data(),
+        accounts: crate::accounts::PreSolGuard {
+            sol_guarded_state,
+            signer,
+            system_program,
+            guarded_account,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct PostSolGuardArgs;
+pub struct PostSolGuardAccounts {
+    pub sol_guarded_state: Pubkey,
+    pub signer: Pubkey,
+    pub guarded_account: Pubkey,
+}
+pub fn post_sol_guard_ix(
+    program_id: Pubkey,
+    _args: PostSolGuardArgs,
+    accounts: PostSolGuardAccounts,
+) -> Instruction {
+    let PostSolGuardAccounts {
+        sol_guarded_state,
+        signer,
+        guarded_account,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: vec![],
+        accounts: crate::accounts::PostSolGuard {
+            sol_guarded_state,
+            signer,
+            guarded_account,
+        }
+        .to_account_metas(None),
+    }
+}
+
+pub struct CloseSolGuardedStateArgs;
+pub struct CloseSolGuardedStateAccounts {
+    pub sol_guarded_state: Pubkey,
+    pub signer: Pubkey,
+    pub guarded_account: Pubkey,
+}
+pub fn close_sol_guarded_state_ix(
+    program_id: Pubkey,
+    _args: CloseSolGuardedStateArgs,
+    accounts: CloseSolGuardedStateAccounts,
+) -> Instruction {
+    let CloseSolGuardedStateAccounts {
+        sol_guarded_state,
+        signer,
+        guarded_account,
+    } = accounts;
+
+    Instruction {
+        program_id,
+        data: vec![],
+        accounts: crate::accounts::CloseSolGuardedState {
+            sol_guarded_state,
+            signer,
+            guarded_account,
+        }
+        .to_account_metas(None),
+    }
+}
